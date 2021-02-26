ac3aaf68f84af3e1fb1e8368ad52171e6de68019
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-02-26 10:26:02 -0500

    pushed some code from yesterday

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
+const inqirer = require('inquirer')
+const path = require('path')
+const fs = require('fs')
+const generateMarkdown = require('./')
 
+//create my array of questions for the user input
+
+const questions = [
+	{
+		type: 'input',
+		name: 'github',
+		message: 'what is your github username?'
+	},
+	{
+		type: 'input',
+		name: 'email',
+		message: 'what is your email address?'
+	},
+	{
+		type: 'list',
+		name: 'license',
+		message: 'What kind of license is your project using?'
+		choices: ['MIT', 'APACHE', 'NONE']
+	}
+]
+
+// Function to write the readME file using the user input
+function writeToFile(fileName, data) {
+	return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+
+//write the function to initialize the application
+function init(){
+	inquirer.prompt(questions).then((inquirerResponses) => {
+	
+})
+
+
+//create a function that returns the license link based on the user pick of none return something.
+//create a function that returns the license section of the readME if there is none return something
+
+function generateMarkdown(data) {
+return `
+	##Description
+	${data.description}
+
+	##Github User
+	${data.github}
+
+	##Github Email
+	${data.email}
+	
+	##User License
+	${data.license}
+
+	`
+}
+
+module.exports = generateMarkdown;
